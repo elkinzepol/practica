b7f20445a0fe6f32dae460ed734be10bf627db8f
PcIntelco <elkinluislopez@gmail.com>
2021-07-29 18:06:25 -0500

    configurando server y app

---
+++ config/APP.php
@@ -0,0 +1,13 @@
+<?php
+    const SERVERURL="http://localhost/sigeosbdi/"; //Url hosting o localhost
+
+    const COMPANY="Intelco"; //Nombre Empresa
+
+    const MONEDA="$"; //Simbolo moneda
+
+    date_default_timezone_set("America/Bogota");//Zona Horaria Bogota- Colombia
+
+
+?>
+
+
